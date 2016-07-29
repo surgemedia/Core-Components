aad8dc07c474846272f7a93c1bf6ebb5c9f562b1
Hector <hectornucv@gmail.com>
2016-07-29 16:52:10 +1000

    update

--- page.php
+++ page.php
@@ -1,4 +1,3 @@
-
 <?php
 if (is_front_page()){ ?>
 <?php 
@@ -29,12 +28,11 @@ if (is_front_page()){ ?>
 														]
 											 ]);
  ?>
-<?php } ?>
-<?php  ?>
-<div class="row">
+ <?php } ?>
+
+
 <?php
 $layout_builder = get_field('layout');
-debug(get_field('layout'));
 //is there block?
 if(isset($layout_builder[0])){
 foreach ($layout_builder as $key => $value) {
@@ -42,11 +40,7 @@ foreach ($layout_builder as $key => $value) {
 	if(isset($section_file)){
 	unset($value['acf_fc_layout']); //of section
 	
-	//Section Options
-	// $value['section_data'] = get_section_options($value);
-	// $value['section_classes'] = 'class="'.$section_file.' '.$value['section_data']['border'].' '.$value['section_data']['background_color'].' '.$value['section_data']['section_width'].' '.$value['section_data']['padding'].' '.$value['section_data']['margin'].' '.$value['section_data']['text_align'].'"';
-	// $value['section_id'] = 'id="'.$value['section_data']['id'].'"';
-	// $value['section_style'] = 'style="background-image:url('.$value['section_data']['background_image'].');"';
+	$value["section"] = $section_file;
 
 
 	//Call file for display
@@ -62,16 +56,16 @@ foreach ($layout_builder as $key => $value) {
 	get_component([
 						'template' => 'template/no-section-warning',
 						'vars' => []
-			]); ?>
-			<section id="" class="white-bg container padding-4 margin-0 text-left" >
- 	<article class="col-md-12  molecule card">
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-		</article>
-</section>
-<?php } ?>
-</div>
+			]);?>
+	<section id="" class="white-bg container padding-4 margin-0 text-left" >
+	 	<article class="col-md-12  molecule card">
+	    <div class="entry-content">
+	      <?php the_content(); ?>
+	    </div>
+	    <footer>
+	      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+	    </footer>
+			</article>
+	</section>	
+<?php }
+ ?>
